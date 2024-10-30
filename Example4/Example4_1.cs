b2e38c98a291f8be76099eaa335c8d54b26c851f
TinyNik0 <98095059+TinyNik0@users.noreply.github.com>
2024-10-30 22:34:51 +0100

    Leave some comments for last example task.

--- Example4/Example4_1.cs
+++ Example4/Example4_1.cs
@@ -3,37 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// View part изменил подписку на нажатие кнопки, чтобы не создавать анонимные методы каждый раз
+// Учитывая что имеет смысл использовать пул объектов, то выключение и включение в такой реализации не будет накапливать подпоски на кнопки
 public class CheatElementBehaviour : MonoBehaviour
 {
     [SerializeField] private Text _text;
     [SerializeField] private Button _button;
 
+    private CheatActionDescription _description;
+    
+    // Устанавливаем описание чита и действие, которое будет выполняться при нажатии на кнопку
     public void Setup(CheatActionDescription description)
     {
         _text.text = description.name;
-        _button.onClick.AddListener(() => description.cheatAction());
+        _description = description;
+    }
+    
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(OnButtonClick);
+    }
+    
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(OnButtonClick);
+    }
+    
+    private void OnButtonClick()
+    {
+        _description?.cheatAction();
     }
 }
 
+// Data part
+public class CheatActionDescription
+{
+    public readonly string name;
+    public readonly Action cheatAction;
+
+    public CheatActionDescription(string name, Action cheatAction)
+    {
+        this.name = name;
+        this.cheatAction = cheatAction;
+    }
+}
+
+public interface ICheatProvider
+{
+    IEnumerable<CheatActionDescription> GetCheatActions();
+}
+
+// Singleton класс для управления отображением панели читов
 public class CheatManager
 {
-    public class CheatActionDescription
-    {
-        public readonly string name;
-        public readonly Action cheatAction;
-
-        public CheatActionDescription(string name, Action cheatAction)
-        {
-            this.name = name;
-            this.cheatAction = cheatAction;
-        }
-    }
-
-    public interface ICheatProvider
-    {
-        IEnumerable<CheatActionDescription> GetCheatActions();
-    }
-
+    // Можно использовать отложенную инициализацию, чтобы не создавать экземпляр CheatManager, если он не понадобится
+    // public static CheatManager Instance => _instance ??= new CheatManager();
     public static readonly CheatManager Instance = new CheatManager();
 
     private readonly List<ICheatProvider> _providers = new List<ICheatProvider>();
@@ -43,17 +67,23 @@ public class CheatManager
 
     private GameObject _panel;
 
+    // Думаю что можно было бы не передавать панель и элементы читов в Setup, а например загружать их из ресурсов, например при первом обращении.
     public void Setup(GameObject panelPrefab, CheatElementBehaviour cheatElementPrefab)
     {
         _panelPrefab = panelPrefab;
         _cheatElementPrefab = cheatElementPrefab;
     }
 
+    // Не хватает по моему метода UnregisterProvider для удаления провайдера, думаю что такое может быть например при переходе между сценами
+    // Хорошей практикой было бы проверить что мы не добавляем один и тот же провайдер несколько раз
     public void RegProvider(ICheatProvider provider)
     {
         _providers.Add(provider);
     }
 
+    // Можно было использовать асинхронную загрузку панели, чтобы не тормозить игру при открытии панели
+    // Также можно было бы использовать пул объектов для панели и элементов читов, чтобы не создавать их каждый раз заново
+    // Достаточно просто скрыть и показать панель, чтобы не создавать ее каждый раз заново, а объекты читов создавать по мере необходимости.
     public void ShowCheatPanel()
     {
         if (_panel != null)
@@ -71,6 +101,7 @@ public class CheatManager
         }
     }
 
+    // Тут вместо удаления все панелей хорошо было бы использовать пул объектов, перед выключением панели данные можно было бы сбросить в исходное состояние
     public void HideCheatPanel()
     {
         if (_panel == null)
@@ -81,6 +112,7 @@ public class CheatManager
     }
 }
 
+// Эта реализация мне нравится больше, поскольку не обязывает нас использовать CheatManager как компонент GO.
 public class SomeManagerWithCheats : CheatManager.ICheatProvider
 {
     private int _health;
@@ -90,6 +122,7 @@ public class SomeManagerWithCheats : CheatManager.ICheatProvider
         CheatManager.Instance.RegProvider(this);
     }
 
+    //Тут мне нравиться что объекты читов создаются по мере необходимости, а не все сразу
     IEnumerable<CheatManager.CheatActionDescription> CheatManager.ICheatProvider.GetCheatActions()
     {
         yield return new CheatManager.CheatActionDescription("Cheat health", () => _health++);
--- Example4/Example4_2.cs
+++ Example4/Example4_2.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
+// View part
 public class CommonCheat : MonoBehaviour
 {
     [SerializeField] private Text _text;
@@ -14,6 +15,7 @@ public class CommonCheat : MonoBehaviour
     }
 }
 
+// Singleton класс для управления отображением панели читов
 public class CheatManager
 {
     public static readonly CheatManager Instance = new CheatManager();
@@ -21,7 +23,7 @@ public class CheatManager
     private GameObject _panel;
 
     public GameObject Panel => _panel;
-
+    
     public void Setup(GameObject panel)
     {
         _panel = panel;
@@ -39,6 +41,7 @@ public class CheatManager
     }
 }
 
+// Эта реаллизация мне нравится меньше, поскольку обязует нас исползовать текущий менедже как компонент GO.
 public class SomeManagerWithCheats : MonoBehaviour
 {
     [SerializeField] private CheatManager.CommonCheat _cheatPrefab;
@@ -47,6 +50,7 @@ public class SomeManagerWithCheats : MonoBehaviour
 
     public void Setup()
     {
+        // Если порядок инициализации будет нарушен и CheatManager.Instance.Panel будет null, то это вызовет NullReferenceException
         var cheat1 = Instantiate(_cheatPrefab, CheatManager.Instance.Panel.transform);
         cheat1.Setup("Cheat health", () => _health++);
         var cheat2 = Instantiate(_cheatPrefab, CheatManager.Instance.Panel.transform);
